82abaecea9d4efca2304dbe535b26110d00b07fa
Velocitypulse <velocitypulse@outlook.fr>
2016-02-29 17:53:12 +0100

    faille d'optimisation trouvee et resolue git add *! beaucoup de taff a faire

--- sources/mandelbrot.c
+++ sources/mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/16 12:04:51 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/02/29 14:56:23 by                  ###   ########.fr       */
+/*   Updated: 2016/02/29 17:52:05 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,11 @@ void		ft_edit_zoom(double *x1, double *x2, double *y1, double *y2,
 {
 	if (keycode == 24) // +
 	{
-		*zoom += 40;
+		*zoom *= 1.5;
 	}
 	else if (keycode == 27) // -
 	{
-		*zoom -= 40;
+		*zoom /= 1.5;
 	}
 	(void)i_max;
 	(void)x1;
@@ -53,39 +53,69 @@ void		ft_mandelbrot(t_hook_info *info)
 	static double	y1 = -1.2;
 	static double	y2 = 1.2;
 
-	static double	zoom = 3000;
+	static double	zoom = 400;
 
 	double	c_r;
 	double	c_i;
 	double	z_r;
 	double	z_i;
 	double	i;
-	static double	i_max = 400;
+	static double	i_max = 32;
 	double	tmp;
 	t_pt	p;
+	t_pt	min;
+	t_pt	max;
+	t_pt	index;
 
 	long int	cpt = 0;
+	long int	cpt2 = 0;
 
-	ft_reset_image(info->current_mlx, 0xffffff);
+	ft_reset_image(info->current_mlx, 0);
 	p = ft_make_pt(0, 0);
+	max = ft_make_pt(0, 0);
+	min = ft_make_pt(0, 0);
 	ft_edit_zoom(&x1, &x2, &y1, &y2, &zoom, &i_max, info->keycode);
 
 	int		image_x = (x2 - x1) * zoom;
 	int		image_y = (y2 - y1) * zoom;
 
-	//	printf("image_x = %d\nimage_y = %d\n\n", image_x, image_y);
-
-	printf("image_x : %d\n", image_x);
-	if (W_WIDTH < image_x)
-		p.x = (W_WIDTH - image_x) / 2;
-	while (p.x < image_x - (image_x - W_WIDTH) / 2)
+	if (image_y > W_HEIGHT)
 	{
-		if (W_HEIGHT < image_y)
-			p.y = (W_HEIGHT - image_y) / 2;
-		while (p.y < image_y - (image_y - W_HEIGHT) / 2)
+		max.y = image_y - ((image_y - W_HEIGHT) / 2);
+		max.x = image_x - ((image_x - W_WIDTH) / 2);
+		min.y = (image_y - W_HEIGHT) / 2;
+		min.x = (image_x - W_WIDTH) / 2;
+	}
+	else
+	{
+		max.y = W_HEIGHT / 2 + image_y / 2;
+		max.x = W_WIDTH / 2 + image_x / 2;
+		min.y = W_HEIGHT / 2 - image_y / 2;
+		min.x = W_WIDTH / 2 - image_x / 2;
+	}
+	printf("dif max.x et min.x : %d\n", max.x - min.x);
+	if (image_x < W_WIDTH)
+		p.x = min.x;
+	else
+		p.x = 0;
+	index.x = min.x - 1;
+	while (++index.x < max.x)
+	{
+		if (image_y < W_HEIGHT)
+			p.y = min.y;
+		else
+			p.y = 0;
+		index.y = min.y - 1;
+		while (++index.y < max.y)
 		{
-			c_r = (double)p.x / zoom + x1;
-			c_i = (double)p.y / zoom + y1;
+			if (image_x < W_WIDTH)
+				c_r = (double)(p.x - min.x) / zoom + x1;
+			else
+				c_r = (double)p.x / zoom + x1;
+			if (image_y < W_HEIGHT)
+				c_i = (double)(p.y - min.y) / zoom + y1;
+			else
+				c_i = (double)p.y / zoom + y1;
 			z_r = 0;
 			z_i = 0;
 			i = 0;
@@ -95,31 +125,30 @@ void		ft_mandelbrot(t_hook_info *info)
 				z_r = z_r * z_r - z_i * z_i + c_r;
 				z_i = 2 * z_i * tmp + c_i;
 				i++;
-				cpt++;
 			}
-			if (i == i_max)
+			if (i >= i_max)
 			{
-				if (W_WIDTH < image_x)
-					ft_draw_pixel(info->current_mlx, 0x000000, ft_make_pt(p.x - (image_x - W_WIDTH) / 2,
-					p.y - (image_y - W_HEIGHT) / 2));
+				if (image_x > W_WIDTH)
+					ft_draw_pixel(info->current_mlx, 0x000000, p);
 				else
 					ft_draw_pixel(info->current_mlx, 0x000000, p);
 			}
 			else
 			{
-				if (W_WIDTH < image_x)
-					ft_draw_pixel(info->current_mlx, ft_get_hexa_rgb(0, i * 255 / i_max, 0), ft_make_pt(
-						p.x - (image_x - W_WIDTH) / 2, p.y - (image_y - W_HEIGHT) / 2));
+				if (image_y > W_HEIGHT)
+					ft_draw_pixel(info->current_mlx, ft_get_hexa_rgb(0, i * 255 / i_max, 0), p);
 				else
 					ft_draw_pixel(info->current_mlx, ft_get_hexa_rgb(0, i * 255 / i_max, 0), p);
 			}
-
 			p.y++;
 		}
 		p.x++;
+		cpt2++;
 	}
+	printf("y : %d\n", p.y);
+	printf("x : %d\n", p.x - (image_x - W_WIDTH) / 2);
 	ft_draw_pixel(info->current_mlx, 0xffffff, ft_make_pt(W_WIDTH / 2, W_HEIGHT / 2));
-	printf("%ld\n", cpt);
+	printf("%ld %ld\n", cpt, cpt2);
 	mlx_do_sync(info->current_mlx);
 	ft_flush_image(info->current_mlx);
 }
