601131db3db7992d12b3ed84d1e4ab6b06fe9fc6
Clement CHAMEYRAT <cchameyr@e3r8p11.42.fr>
2016-04-03 12:16:34 +0200

    mise a jour de la libft

--- libft/ft_add_ptd.c
+++ libft/ft_add_ptd.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strequ.c                                        :+:      :+:    :+:   */
+/*   ft_add_ptd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cchameyr <cchameyr@students.42.fr>         +#+  +:+       +#+        */
+/*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/27 16:34:42 by cchameyr          #+#    #+#             */
-/*   Updated: 2015/11/28 10:58:10 by cchameyr         ###   ########.fr       */
+/*   Created: 2016/04/03 12:09:02 by cchameyr          #+#    #+#             */
+/*   Updated: 2016/04/03 12:10:38 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strequ(char const *s1, char const *s2)
+t_ptd		ft_add_ptd(t_ptd pt1, t_ptd pt2)
 {
-	if (s1 && s2)
-	{
-		if (ft_strcmp(s1, s2) == 0)
-			return (1);
-	}
-	else if (s1 == s2)
-		return (1);
-	return (0);
+	t_ptd		pt3;
+
+	pt3.x = pt1.x + pt2.x;
+	pt3.y = pt1.y + pt2.y;
+	return (pt3);
 }
--- libft/ft_make_ptd.c
+++ libft/ft_make_ptd.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strequ.c                                        :+:      :+:    :+:   */
+/*   ft_make_ptd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cchameyr <cchameyr@students.42.fr>         +#+  +:+       +#+        */
+/*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/27 16:34:42 by cchameyr          #+#    #+#             */
-/*   Updated: 2015/11/28 10:58:10 by cchameyr         ###   ########.fr       */
+/*   Created: 2016/04/03 12:07:13 by cchameyr          #+#    #+#             */
+/*   Updated: 2016/04/03 12:08:20 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strequ(char const *s1, char const *s2)
+t_ptd		ft_mak_ptd(double x, double y)
 {
-	if (s1 && s2)
-	{
-		if (ft_strcmp(s1, s2) == 0)
-			return (1);
-	}
-	else if (s1 == s2)
-		return (1);
-	return (0);
+	t_ptd		pt;
+
+	pt.x = x;
+	pt.y = y;
+	return (pt);
 }
